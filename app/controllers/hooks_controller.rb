86dce7ca230f7d2f73f8c7d8b28bf7f69609c035
Jon Allured <jon.allured@gmail.com>
2016-09-29 10:48:38 -0500

    Hack in some payload parsing for Travis hooks

--- app/controllers/hooks_controller.rb
+++ app/controllers/hooks_controller.rb
@@ -11,8 +11,14 @@ class HooksController < ApplicationController
   private
 
   def hook_params
+    payload = JSON.parse(params[:payload])
+    message = "Build ##{payload['number']} of #{payload['repository']['owner_name']}/#{payload['repository']['name']}@#{payload['branch']} by #{payload['author_name']} #{payload['result_message']}."
+    url = payload['build_url']
+
     {
-      payload: JSON.parse(params[:payload]),
+      payload: payload,
+      message: message,
+      url: url,
       sent_at: Time.now
     }
   end
--- spec/requests/hooks/travis_spec.rb
+++ spec/requests/hooks/travis_spec.rb
@@ -3,9 +3,15 @@ require 'rails_helper'
 describe 'Travis hooks' do
   it 'something' do
     payload = {
-      id: 1,
       number: "1",
-      status: 0
+      branch: "master",
+      author_name: 'Jon Allured',
+      result_message: 'Passed',
+      build_url: 'https://travis-ci.org/',
+      repository: {
+        owner_name: 'jonallured',
+        name: 'uplink-rails'
+      }
     }
 
     params = {
@@ -15,5 +21,9 @@ describe 'Travis hooks' do
     post '/hooks', params: params
 
     expect(Hook.count).to eq 1
+
+    hook = Hook.first
+    expect(hook.message).to eq "Build #1 of jonallured/uplink-rails@master by Jon Allured Passed."
+    expect(hook.url).to eq "https://travis-ci.org/"
   end
 end
