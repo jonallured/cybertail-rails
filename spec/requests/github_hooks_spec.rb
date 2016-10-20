bac8e637b1612b3631a652d462dd282b4fc18304
Jon Allured <jon.allured@gmail.com>
2016-10-20 15:23:08 -0500

    Use unknown event for general parser test
    I'm about to add a parser for the ping event, so let's not use that
    event name.

--- spec/requests/github_hooks_spec.rb
+++ spec/requests/github_hooks_spec.rb
@@ -1,26 +1,29 @@
 require 'rails_helper'
 
 describe 'GitHub hooks' do
-  it 'something' do
-    github_service = Service.create name: 'GitHub'
+  context 'unknown event' do
+    it 'creates a general hook' do
+      github_service = Service.create name: 'GitHub'
 
-    params = {
-      repository: {
-        full_name: 'jonallured/uplink-rails'
+      params = {
+        repository: {
+          full_name: 'jonallured/uplink-rails'
+        }
       }
-    }
 
-    headers = { 'X-GitHub-Event' => 'ping' }
+      headers = { 'X-GitHub-Event' => 'unknown' }
 
-    post '/github_hooks', params: params, headers: headers
+      post '/github_hooks', params: params, headers: headers
 
-    expect(Hook.count).to eq 1
+      expect(Hook.count).to eq 1
 
-    hook = Hook.first
-    expect(hook.service).to eq github_service
-    expect(hook.project).to eq "jonallured/uplink-rails"
-    expect(hook.message).to eq "Got event ping, saved as Hook #{hook.id}."
-    expect(hook.url).to eq "https://github.com"
+      hook = Hook.first
+      expect(hook).to_not be_suppress
+      expect(hook.service).to eq github_service
+      expect(hook.project).to eq "jonallured/uplink-rails"
+      expect(hook.message).to eq "Got event unknown, saved as Hook #{hook.id}."
+      expect(hook.url).to eq "https://github.com"
+    end
   end
 
   context 'create event' do
