d3d5dba45ad8eef39b5ef64cb1aec27c3b6c2f54
Jon Allured <jon.allured@gmail.com>
2016-10-31 11:50:12 -0500

    Add seeds file
    Closes #8

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+services = [
+  { id: 1, name: 'Travis CI' },
+  { id: 2, name: 'Heroku' },
+  { id: 3, name: 'GitHub' },
+  { id: 4, name: 'Honeybadger' }
+]
+
+for attrs in services
+  Service.create(attrs) unless Service.exists?(attrs)
+end
