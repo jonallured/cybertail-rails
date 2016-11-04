f3a13252a305d53bc65767cfee8472a35f237d1b
Jon Allured <jon.allured@gmail.com>
2016-11-04 16:33:17 -0500

    Validate hook API endpoint calls
    This is a baby step in the direction of supporting multiple users.

--- app/controllers/api/v1/hooks_controller.rb
+++ app/controllers/api/v1/hooks_controller.rb
@@ -1,3 +1,14 @@
 class Api::V1::HooksController < ApplicationController
+  before_action :ensure_user
   expose(:hooks) { Hook.where(suppress: false) }
+
+  private
+
+  def current_user
+    @current_user ||= User.find_by token: params[:token]
+  end
+
+  def ensure_user
+    head :not_found unless current_user
+  end
 end
--- spec/requests/api/hooks_spec.rb
+++ spec/requests/api/hooks_spec.rb
@@ -4,8 +4,9 @@ describe 'GET /v1/hooks' do
   it 'something' do
     travis_service = Service.create name: 'Travis CI'
     hook = FactoryGirl.create :hook, service: travis_service
+    user = FactoryGirl.create :user
 
-    get '/v1/hooks.json'
+    get '/v1/hooks.json', params: { token: user.token }
 
     expect(response.code).to eq '200'
 
