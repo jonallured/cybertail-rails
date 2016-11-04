d73b40ba3d96bd95627b6c8b0d64c607e4fcd127
Jon Allured <jon.allured@gmail.com>
2016-11-04 17:01:35 -0500

    Add missing specs
    These specs ensure some safety on the hooks endpoint.

--- spec/requests/api/hooks_spec.rb
+++ spec/requests/api/hooks_spec.rb
@@ -1,24 +1,50 @@
 require 'rails_helper'
 
 describe 'GET /v1/hooks' do
-  it 'something' do
-    travis_service = Service.create name: 'Travis CI'
-    hook = FactoryGirl.create :hook, service: travis_service
-    user = FactoryGirl.create :user
+  context 'without a token' do
+    it 'it returns an empty 404 response' do
+      travis_service = Service.create name: 'Travis CI'
+      hook = FactoryGirl.create :hook, service: travis_service
 
-    get '/v1/hooks.json', params: { token: user.token }
+      get '/v1/hooks.json'
 
-    expect(response.code).to eq '200'
+      expect(response.code).to eq '404'
+      expect(response.body).to eq ''
+    end
+  end
 
-    response_json = JSON.parse response.body
-    expect(response_json).to eq([
-      {
-        'service_id' => travis_service.id,
-        'project' => hook.project,
-        'message' => hook.message,
-        'url' => hook.url,
-        'sent_at' => hook.sent_at.as_json
-      }
-    ])
+  context 'with an invalid token' do
+    it 'it returns an empty 404 response' do
+      travis_service = Service.create name: 'Travis CI'
+      hook = FactoryGirl.create :hook, service: travis_service
+
+      get '/v1/hooks.json', params: { token: 'invalid' }
+
+      expect(response.code).to eq '404'
+      expect(response.body).to eq ''
+    end
+  end
+
+  context 'with a valid token' do
+    it 'something' do
+      travis_service = Service.create name: 'Travis CI'
+      hook = FactoryGirl.create :hook, service: travis_service
+      user = FactoryGirl.create :user
+
+      get '/v1/hooks.json', params: { token: user.token }
+
+      expect(response.code).to eq '200'
+
+      response_json = JSON.parse response.body
+      expect(response_json).to eq([
+        {
+          'service_id' => travis_service.id,
+          'project' => hook.project,
+          'message' => hook.message,
+          'url' => hook.url,
+          'sent_at' => hook.sent_at.as_json
+        }
+      ])
+    end
   end
 end
